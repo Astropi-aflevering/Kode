fe78f71f9087e6f18d0c6726af40817eeb6081a3
Love-strawberry12 <majo0001@brgym.dk>
2025-12-02 10:48:17 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -3,11 +3,10 @@
 
 # https://projects.raspberrypi.org/en/projects/mission-space-lab-creator-guide/2
 # Import the Camera class from the picamera-zero module
-#der skal sættes et standard format for billederne og filerne, som gør at de kan indeholde tidspunkter, lokationer og type af kamera
-from exif import Image
-from datetime import datetime
-import cv2
-import math
+from exif import Image # Det er den der gør vi kan hente et billede, den bruger en funktion til at indsætte det og giver mulighed for at læse exif-data som tidspunkt og GPS osv...
+from datetime import datetime # det er en funktion som henter pakkerne og bruges til at konvertere tekst til datatime- objekter.
+import cv2 #bruges ofte til billedebehandling
+import math # bruges til matematiske beregninger
 
 from picamzero import Camera
 
@@ -15,98 +14,135 @@ from picamzero import Camera
 cam = Camera()
 
 # Capture an image
+#koden nedenfor, er en funktion til at kunne tage billeder oppefra og bruge dem yderligere til ting.
 
 cam.take_photo("image1.jpg")
 cam.take_photo("image2.jpg")
 cam.take_photo("image2.jpg")
 cam.take_photo("image2.jpg")
 cam.take_photo("image2.jpg")
-#der skal tages billeder af jorden fra rummet det
-#de her billeder er dem vi skal se nærmere på omlidt
+
 
 cam.take_photo("image2.jpg")
 
 
 image_1 = 'image1.jpg'
 image_2 = 'image2.jpg'
-#her har man navngivet billederne, så man blot kan kalde dem for image_1 og image_2
 
-def get_time(image):
-#billderne skal åbnes og herefter blive konverteret til et objekt-
-    with open(image, 'rb') as image_file:
+"""
+ Funktionen åbner et billede og læser de skjulte oplysninger,som kameraet har gemt i filen.
+ Den viser først alle de oplysninger, der findes, og derefter finder den den linje, hvor tidspunktet for billedet står.
+ Til sidst laver den tidspunktet om til et format, som computeren kan regne med, og giver det tilbage.
+
+"""
+
+def get_time(image):    with open(image, 'rb') as image_file:
         img = Image(image_file)
         for data in img.list_all():
             print(data)
-#Her har man nu mulighed for at se på al Exif dataen som er gemt i billedet
             time_str = img.get("datetime_original")
         time = datetime.strptime(time_str, '%Y:%m:%d %H:%M:%S')
     return time
-#Man skal gemme tidsdataen, så der kan blive lavet udregninger på det
 #print(get_time('photo_0683.jpg'))
-#nu skal vi finde tidsforskellen for de to billeder
-def get_time_difference(image_1, image_2):
-#man får tiden fra exif dataen og så trækker man dem fra hinanden for at finde tidsforskellen
+
+"""
+Funktionen finder først tidspunktet for det første billede og derefter tidspunktet for det andet billede.
+Så trækker den det første tidspunkt fra det andet for at finde ud af, hvor lang tid der gik mellem de to billeder.
+Til sidst giver funktionen antallet af sekunder tilbage.
+"""
+
+def get_time_difference(image_1, image_2): 
     time_1 = get_time(image_1)
     time_2 = get_time(image_2)
     time_difference = time_2 - time_1
-#man vil gerne have tiden i sekunder, så det gør koden nedenunder for os
     return time_difference.seconds
 #    print(time_difference)
-#nu skal vi have fundet de "matchende" egenskaber ved de to billeder
-#her skal vi bruge cv2 pakken
-#billederne skal konverteres til OpenCV objekter, så de kan blive processed, derfor skal man tilføje en funktion som
-#tager de to billeder og retunerer dem som objekter
+
+
+
+"""
+Funktionen åbner de to billeder, men gør dem om til sort-hvid, så computeren lettere kan arbejde med dem.
+Derefter giver den de to sort-hvide versioner tilbage, så de kan bruges i andre beregninger.
+"""
+
 def convert_to_cv(image_1, image_2):
     image_1_cv = cv2.imread(image_1, 0)
     image_2_cv = cv2.imread(image_2, 0)
     return image_1_cv, image_2_cv
-#Disse "OpenCV" objekter kan blive brugt af ORB algoritmen. Denne algoritme vil opfange "keypoints" på et eller flere billeder
-#hvis billederne er tæt på at være ens, vil de samme "keypoints" blive opdaget, selvom de har rykket sig eller ændret sig lidt
-#ORB kan faktisk også laver "descriptors" til disse "keypoints", de vil indeholde informationer som position, størrelse, rotation og lysstyrke
-#Ved at sammenligne disse "descriptors" mellem "keypoints" kan man beregne ændringerne fra det ene billede til det andet
-#vi skal nu derfor finde "keypointsne" og "descriptors" for de 2 billeder
+
+
+"""
+Funktionen bruger en metode, der kan finde særlige punkter i et billede, lidt som små genkendelige mærker.
+Den leder efter sådan nogle punkter i begge billeder og beskriver dem med tal, så computeren kan sammenligne dem.
+Til sidst giver funktionen alle de fundne punkter og beskrivelser tilbage, så man senere kan bruge dem til at se,
+hvordan billederne har flyttet sig i forhold til hinanden.
+"""
+
 def calculate_features(image_1, image_2, feature_number):
     orb = cv2.ORB_create(nfeatures = feature_number)
     keypoints_1, descriptors_1 = orb.detectAndCompute(image_1_cv, None)
     keypoints_2, descriptors_2 = orb.detectAndCompute(image_2_cv, None)
-    return keypoints_1, keypoints_2, descriptors_1, descriptors_2
-#Den nemmeste måde at sammenligne "descriptors" for de 2 billeder er ved at bruge brute force
-#Brute force er en algoritme som prøver alle mulige kombinationer ved at matche en "descriptor fra det ene billede med alle de andre "descriptors" fra det andet billede
-#Her bliver der derfor skrevet en funktion som tager de set af descriptors og prøver at finde matches vha. brute force
+  return keypoints_1, keypoints_2, descriptors_1, descriptors_2
+
+
+
+"""
+Funktionen sammenligner de to billeders beskrivelser af punkter for at finde ud af, hvilke der ligner hinanden.
+Den bruger en metode, der matcher punkter fra det ene billede med punkter fra det andet.
+Til sidst sorterer den matchene, så de bedste og mest præcise står øverst, og giver dem tilbage.
+"""
+
 def calculate_matches(descriptors_1, descriptors_2):
     brute_force = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
     matches = brute_force.match(descriptors_1, descriptors_2)
     matches = sorted(matches, key=lambda x: x.distance)
     return matches
-#Man kan også få spillet til at vise matches det er gjort i nedenstående kode
+
+
+
+"""
+Funktionen tager to billeder og de punkter, der blev fundet i dem, samt oplysninger om hvilke punkter, der matcher mellem billederne.
+Den tegner streger mellem alle de bedste matchende punkter, så man kan se, hvilke dele af det første billede,
+der svarer til det andet billede.
+Billedet bliver gjort stort nok til at kunne ses på skærmen,
+og derefter viser computeren det i et vindue. Når man lukker vinduet, fjernes billedet igen.
+"""
+
 def display_matches(image_1_cv, keypoints_1, image_2_cv, keypoints_2, matches):
     match_img = cv2.drawMatches(image_1_cv, keypoints_1, image_2_cv, keypoints_2, matches[:100], None)
-#Her bliver billederne give en ny størrelse og sat op side om side med linjer mellem matches    
     resize = cv2.resize(match_img, (1600,600), interpolation = cv2.INTER_AREA)
     cv2.imshow('matches', resize)
-#Man skal også kunne lukke billedet ned igen, derfor har man gjort at hvis man trykker på "0" lukker det ned    
     cv2.waitKey(0)
     cv2.destroyWindow('matches')
 
-#nu skal vi finde koordinaterne til matches
-#først laver vi en funktion som tager de to sæt af keypoints og lister af matches som "arguments"
+
+
+"""
+Funktionen går igennem alle de punkter, der matcher mellem de to billeder, og finder de præcise positioner (x- og y-koordinater)
+af hvert punkt i begge billeder. Den samler alle koordinaterne fra det første billede i én liste og alle koordinaterne fra det
+andet billede i en anden liste. Til sidst giver den begge lister tilbage, så man kan bruge dem til at beregne, hvordan billederne
+har bevæget sig i forhold til hinanden.
+"""
 def find_matching_coordinates(keypoints_1, keypoints_2, matches):
-#nu skal man lave 2 tomme lister som skal indeholde koordinaterne for hver af de matchende egenskaber for billederne    
     coordinates_1 = []
     coordinates_2 = []
-#listerne for matches indeholder mange objekter.
-#man kan køre gennem listen gentagene gange for at finde koordinaterne til match på billedet
-#Derfor tilføjer vi et loop, for at hente koordinaterne til hver match
     for match in matches:
         image_1_idx = match.queryIdx
         image_2_idx = match.trainIdx
         (x1,y1) = keypoints_1[image_1_idx].pt
         (x2,y2) = keypoints_2[image_2_idx].pt
-#så skal vi have tilføjet koordinaterne til de to koordinatlister
         coordinates_1.append((x1,y1))
         coordinates_2.append((x2,y2))
-#de to lister kan nu blive retuneret
     return coordinates_1, coordinates_2
+
+
+"""
+Funktionen tager de to lister med koordinater fra de matchende punkter i de to billeder.
+Den regner afstanden mellem hvert par af punkter og lægger alle afstandene sammen.
+Til sidst finder den gennemsnittet, altså den typiske afstand mellem punkterne, så man kan se,
+hvor meget billederne har forskudt sig i forhold til hinanden.
+"""
+
 
 def calculate_mean_distance(coordinates_1, coordinates_2):
     all_distances = 0
@@ -120,12 +156,23 @@ def calculate_mean_distance(coordinates_1, coordinates_2):
     #print(coordinates_1[0])
     #print(coordinates_2[0])
     #print(merged_coordinates[0])
+
+
+"""
+Funktionen tager gennemsnitsafstanden mellem punkterne i billederne og omregner den til rigtige kilometer ved
+hjælp af GSD-værdien (som fortæller, hvor stor en pixel er i virkeligheden). Derefter deler den afstanden med
+den tid, der gik mellem billederne, og finder på den måde, hvor hurtigt noget bevægede sig i kilometer per sekund.
+Til sidst giver funktionen hastigheden tilbage.
+"""
+
+
 def calculate_speed_in_kmps(feature_distance, GSD, time_difference):
     distance = feature_distance * GSD / 100000
     speed = distance / time_difference
     return speed
 
 
+# her kører programmet.
 
 image_1_cv, image_2_cv = convert_to_cv(image_1, image_2) # Create OpenCV image objects
 
@@ -141,16 +188,6 @@ average_feature_distance = calculate_mean_distance(coordinates_1, coordinates_2)
 speed = calculate_speed_in_kmps(average_feature_distance, 12648, time_difference)
 
 
-#timedif=get_time_difference('image1.jpg', 'image2.jpg')
-
-
-# Format the estimate_kmps to have a precision
-# of 5 significant figures
-#timedifFormatted = "{:.3f}".format(timedif)
-
-#file_path = "result.txt"  # Replace with your desired file path
-#with open(file_path, 'w') as file:
-#    file.write(timedifFormatted)
 
 estimate_kmps = speed  # Replace with your estimate
 
